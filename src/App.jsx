92456ef3a004769e8cef47f4d72a445e646252e1
NuReach <nureach46@gmail.com>
2024-02-13 23:14:52 +0700

    login and signup page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import News from "./pages/News"
 import Profile from "./pages/Profile"
 import User from "./pages/User"
 import Playing from "./pages/Playing"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/user" element={<User />} />
         <Route path="/quiz/playing/:id" element={<Playing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
 
