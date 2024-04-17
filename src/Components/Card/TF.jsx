6a76a1b40804eb3785adaefe8b0dc59175d379b0
NuReach <nureach46@gmail.com>
2024-04-17 22:44:35 +0700

    preview answer

--- src/Components/Card/TF.jsx
+++ src/Components/Card/TF.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function TF({item,number}) {
+export default function TF({item,number,userChoice}) {
   return (
-    <div className=' w-full md:w-96 lg:w-1/3'>
+    <div className=' w-full '>
       <div className=''>
       <p className='my-2 text-sm font-medium capitalize'>{number}.{item?.question.question_prompt}</p>
       </div>
@@ -22,6 +22,19 @@ export default function TF({item,number}) {
             ))
           }
       </div>
+      <div className='w-full flex justify-start flex-col gap-3 mt-3'>
+          <p className='text-xs font-bold'>Your Answer</p>
+          <div className={userChoice.is_correct === 1 ? 'w-full p-2 border-4 rounded-lg border-green-500 text-xs bg-black bg-opacity-5 hover:bg-opacity-0 transition' :'w-full p-2 text-xs border-4 border-red-500 rounded-lg bg-black bg-opacity-5 hover:bg-opacity-0 transition '}>
+                <div className='flex items-center space-x-3 p-1'>
+                {
+                  userChoice.is_correct === 1  ? <svg className='text-blue-500 font-medium' xmlns="http://www.w3.org/2000/svg" width="15" height="18" viewBox="0 0 15 15"><path fill="none" stroke="currentColor" d="M4 7.5L7 10l4-5m-3.5 9.5a7 7 0 1 1 0-14a7 7 0 0 1 0 14Z"/></svg>
+                : <svg xmlns="http://www.w3.org/2000/svg" width="15" height="18" viewBox="0 0 32 32"><circle cx="16" cy="16" r="14" fill="currentColor"/></svg>
+                
+                }
+                <p className='font-medium capitalize'> {userChoice.text}</p>
+                </div>
+            </div>
+          </div>
   </div>
   )
 }
--- src/pages/AnswerPreview.jsx
+++ src/pages/AnswerPreview.jsx
@@ -23,14 +23,53 @@ export default function AnswerPreview() {
       <Navbar />
         <div className='flex'>
             <Sidebar />
-            <div className='p-3 w-full justify-center flex flex-wrap gap-6' >
+            <div className='p-3 w-full justify-center items-center flex gap-6 flex-col' >
+              <div className='lg:w-2/3 md:w-96 w-96 flex flex-col justify-start'>
+                <div className='w-full md:w-96  flex gap-3'>
+                  <p className='text-sm font-bold capitalize w-16'>Exam</p>
+                  <p className='text-sm font-medium capitalize '>{userResult?.exam.exam_title}</p>
+                </div>
+                <div className='w-full md:w-96  flex gap-3'>
+                  <p className='text-sm font-bold capitalize w-16 '>Course</p>
+                  <p className='text-sm font-medium capitalize '>{userResult?.exam.course.course_title}</p>
+                </div>
+                <div className='w-full md:w-96  flex gap-3'>
+                  <p className='text-sm font-bold capitalize w-16'>Duration</p>
+                  <p className='text-sm font-medium '>{userResult?.exam.exam_duration} mn</p>
+                </div>
+                <div className='w-full md:w-96  flex gap-3'>
+                  <p className='text-sm font-bold capitalize w-16 '>Score</p>
+                  <p className='text-sm font-medium capitalize '>{userResult?.exam.exam_score} points</p>
+                </div>
+                <div className='w-full md:w-96  flex gap-3'>
+                  <p className='text-sm font-bold capitalize w-16 '>Questions</p>
+                  <p className='text-sm font-medium capitalize '>{userResult?.total_questions} questions</p>
+                </div>
+                <div className='w-full md:w-96  flex gap-3'>
+                  <p className='text-sm font-bold capitalize w-16 '>Minimum</p>
+                  <p className='text-sm font-medium capitalize '>{userResult?.exam.exam_percentage} %</p>
+                </div>
+                <div className='w-full md:w-96  flex gap-3 border-t-2 mt-6 pt-3'>
+                  <p className='text-sm font-bold capitalize '>Your Result</p>
+                  <p className='text-sm font-medium capitalize '>{userResult?.total_is_correct*100/userResult?.total_questions}%</p>
+                </div>
+                {
+                  userResult?.total_missing > 0 &&
+                <div className='w-full md:w-96  flex gap-3 text-red-600'>
+                  <p className='text-sm font-bold capitalize w-16 '>Missing</p>
+                  <p className='text-sm font-medium capitalize '>{userResult?.total_missing} quesitons</p>
+                </div>
+                }
+              </div>
+              <div className=' lg:w-2/3 md:w-96 w-96 flex flex-wrap gap-14'>
                 {
                  userResult?.answers.map((item,i)=>(
                   item.question.question_type == "multiple choice" ?
-                  <QCM key={i} item={item} number={i+1} /> :
-                  <TF key={i} item={item} number={i+1} />
+                  <QCM key={i} item={item} userChoice={item?.choice} number={i+1} /> :
+                  <TF key={i} item={item} userChoice={item?.choice}  number={i+1} />
                  ))
                 }
+              </div>
             </div>
         </div>
       <Footer />
