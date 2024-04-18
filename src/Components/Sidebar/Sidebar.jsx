9a95edc47b3ad38b06f797270b928acac0eb2e60
NuReach <nureach46@gmail.com>
2024-04-18 13:50:06 +0700

    bar  desgin

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -35,17 +35,17 @@ export default function Sidebar() {
   const channels = [
         {
             title:"Facebook",
-            link:"/facebook",
+            link:"https://www.facebook.com/LimkokwingCambodia",
             icon:'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"/></svg>',
         },
         {
             title:"Youtube",
-            link:"/youtube",
+            link:"https://www.youtube.com/@LimkokwingUniversityOfficial",
             icon:'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="currentColor" d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104l.022.26l.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105l-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006l-.087-.004l-.171-.007l-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103l.003-.052l.008-.104l.022-.26l.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007l.172-.006l.086-.003l.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/></svg>',
         },
         {
             title:"Website",
-            link:"/website",
+            link:"https://www.limkokwing.net/cambodia/",
             icon:'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M4 20q-.825 0-1.412-.587T2 18V6q0-.825.588-1.412T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.587 1.413T20 20zm0-2h10.5v-3.5H4zm12.5 0H20V9h-3.5zM4 12.5h10.5V9H4z"/></svg>',
         },
     
@@ -66,10 +66,10 @@ export default function Sidebar() {
         <p className='font-bold text-gray-500 text-sm hidden md:block mt-3'>Channels</p>
             {
                 channels.map((item,i)=>(
-                    <div key={i} className={ route==item.link ?'text-white bg-black p-3 rounded-md font-bold flex space-x-3 justify-center md:justify-normal items-center text-sm cursor-pointer':'text-gray-600 justify-center md:justify-normal font-bold flex space-x-3 items-center text-sm cursor-pointer'}>
+                    <a  href={item.link} target='_blank' key={i} className={ route==item.link ?'text-white bg-black p-3 rounded-md font-bold flex space-x-3 justify-center md:justify-normal items-center text-sm cursor-pointer':'text-gray-600 justify-center md:justify-normal font-bold flex space-x-3 items-center text-sm cursor-pointer'}>
                         <div dangerouslySetInnerHTML={{ __html: item.icon }} />
                         <p  className='hidden md:block'>{item.title}</p>
-                    </div>
+                    </a>
                 ))
             }
         </section>
